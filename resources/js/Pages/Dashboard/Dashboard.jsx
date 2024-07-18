0942cf20447d0b542729364492641bac33b31dbc
Michael-Nanpa <michaelwijayananpa320@gmail.com>
2024-07-18 22:22:23 +0700

    Update Dashboard.jsx

--- resources/js/Pages/Dashboard/Dashboard.jsx
+++ resources/js/Pages/Dashboard/Dashboard.jsx
@@ -1,4 +1,4 @@
-import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout_Admin';
 import { Head } from '@inertiajs/react';
 
 export default function Dashboard({ auth }) {
