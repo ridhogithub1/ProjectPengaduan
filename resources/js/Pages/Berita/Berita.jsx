22f06d08374a0fc9b709a75062b208d69641fa9d
kenst4b <136431799+kenst4b@users.noreply.github.com>
2024-07-18 20:49:27 +0700

    Update Berita.jsx
    errmm

--- resources/js/Pages/Berita/Berita.jsx
+++ resources/js/Pages/Berita/Berita.jsx
@@ -1,127 +1,53 @@
 import React from 'react';
 import { Link } from '@inertiajs/react';
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
+import Slider from 'react-slick';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import BeritaItem from './BeritaItem';
 
-export default function Berita({ berita }) {
+export default function Berita({ berita, carousel }) {
+
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+    };
+
     return (
         <AuthenticatedLayout>
             <main className="h-full pb-16 overflow-y-auto">
                 <div className="container grid px-6 mx-auto">
-                <div className="flex justify-between items-center my-6">
+                    <div className="flex justify-between items-center my-6">
                         <h2 className="text-2xl font-semibold text-gray-700 dark:text-gray-200">
                             Berita
                         </h2>
-                        <Link href="/Pengaduan/create" className="px-4 py-2 bg-purple-600 text-white rounded-lg hover:bg-purple-700">
+                        <Link href="/berita/create" className="px-4 py-2 bg-purple-600 text-white rounded-lg hover:bg-purple-700">
                             Tambah Berita
                         </Link>
                     </div>
-                    <div className="w-full mb-8 overflow-hidden rounded-lg shadow-xs">
-                        <div className="w-full overflow-x-auto">
-                            <table className="w-full whitespace-no-wrap">
-                                <thead>
-                                    <tr className="text-xs font-semibold tracking-wide text-left text-gray-500 uppercase border-b dark:border-gray-700 bg-gray-50 dark:text-gray-400 dark:bg-gray-800">
-                                        <th className="px-4 py-3">No</th>
-                                        <th className="px-4 py-3">Judul</th>
-                                        <th className="px-4 py-3">Isi</th>
-                                        <th className="px-4 py-3">Image</th>
-                                        <th className="px-4 py-3">Tanggal</th>
-                                        <th className="px-4 py-3">Aksi</th>
-                                    </tr>
-                                </thead>
-                                <tbody className="bg-white divide-y dark:divide-gray-700 dark:bg-gray-800">
-                                    {berita.map((item, index) => (
-                                        <tr className="text-gray-700 dark:text-gray-400" key={item.id}>
-                                            <td className="px-4 py-3 ">{index + 1}</td>
-                                            <td className="px-4 py-3">
-                                                <div className="flex items-center text-sm">
-                                                    <p className="font-semibold">{item.judul}</p>
-                                                </div>
-                                            </td>
-                                            <td className="px-4 py-3 text-sm">{item.isi}</td>
-                                            <td className="px-4 py-3">
-                                                <div className="flex items-center text-sm">
-                                                    <img className="object-cover w-20 h-20" src={item.image} alt="" loading="lazy" />
-                                                </div>
-                                            </td>
-                                            <td className="px-4 py-3 text-sm">{item.tanggal}</td>
-                                            <td className="px-4 py-3 text-xs">
-                                                <Link href={`Edit/${item.id}`}
-                                                    className="px-2 py-1 font-semibold leading-tight text-gray-700 bg-gray-100 rounded-full dark:text-gray-100 dark:bg-gray-700">
-                                                    Edit
-                                                </Link>
-                                                <Link href={`Delete/${item.id}`}
-                                                    className="px-2 py-1 font-semibold leading-tight text-red-700 bg-red-100 rounded-full dark:text-red-100 dark:bg-red-700">
-                                                    Delete
-                                                </Link>
-                                            </td>
-                                        </tr>
-                                    ))}
-                                </tbody>
-                            </table>
-                        </div>
-                        <div className="grid px-4 py-3 text-xs font-semibold tracking-wide text-gray-500 uppercase border-t dark:border-gray-700 bg-gray-50 sm:grid-cols-9 dark:text-gray-400 dark:bg-gray-800">
-                            <span className="flex items-center col-span-3">
-                                Showing 21-30 of 100
-                            </span>
-                            <span className="col-span-2"></span>
-                            <span className="flex col-span-4 mt-2 sm:mt-auto sm:justify-end">
-                                <nav aria-label="Table navigation">
-                                    <ul className="inline-flex items-center">
-                                        <li>
-                                            <button className="px-3 py-1 rounded-md rounded-l-lg focus:outline-none focus:shadow-outline-purple"
-                                                aria-label="Previous">
-                                                <svg aria-hidden="true" className="w-4 h-4 fill-current" viewBox="0 0 20 20">
-                                                    <path d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
-                                                        clipRule="evenodd" fillRule="evenodd"></path>
-                                                </svg>
-                                            </button>
-                                        </li>
-                                        <li>
-                                            <button className="px-3 py-1 rounded-md focus:outline-none focus:shadow-outline-purple">
-                                                1
-                                            </button>
-                                        </li>
-                                        <li>
-                                            <button className="px-3 py-1 rounded-md focus:outline-none focus:shadow-outline-purple">
-                                                2
-                                            </button>
-                                        </li>
-                                        <li>
-                                            <button
-                                                className="px-3 py-1 text-white transition-colors duration-150 bg-purple-600 border border-r-0 border-purple-600 rounded-md focus:outline-none focus:shadow-outline-purple">
-                                                3
-                                            </button>
-                                        </li>
-                                        <li>
-                                            <button className="px-3 py-1 rounded-md focus:outline-none focus:shadow-outline-purple">
-                                                4
-                                            </button>
-                                        </li>
-                                        <li>
-                                            <span className="px-3 py-1">...</span>
-                                        </li>
-                                        <li>
-                                            <button className="px-3 py-1 rounded-md focus:outline-none focus:shadow-outline-purple">
-                                                8
-                                            </button>
-                                        </li>
-                                        <li>
-                                            <button className="px-3 py-1 rounded-md focus:outline-none focus:shadow-outline-purple">
-                                                9
-                                            </button>
-                                        </li>
-                                        <li>
-                                            <button className="px-3 py-1 rounded-md rounded-r-lg focus:outline-none focus:shadow-outline-purple"
-                                                aria-label="Next">
-                                                <svg className="w-4 h-4 fill-current" aria-hidden="true" viewBox="0 0 20 20">
-                                                    <path d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-                                                        clipRule="evenodd" fillRule="evenodd"></path>
-                                                </svg>
-                                            </button>
-                                        </li>
-                                    </ul>
-                                </nav>
-                            </span>
+                    <div className="w-full mb-8 overflow-hidden  shadow-xs">
+                        <Slider {...settings}>
+                            {carousel.map((item, index) => (
+                                <BeritaItem key={index} item={item} />
+                            ))}
+                        </Slider>
+
+                        <h3 className='font-bold text-3xl mt-10'>Lainnya</h3>
+                        <div className="w-full flex flex-wrap justify-between">
+                            {berita.map((item, index) => (
+                                <div key={index} className='w-1/2 p-5 relative mb-5'>
+                                    <img src={item.image_url} alt={item.judul} className='w-full h-[350px] object-cover' />
+                                    <div className='absolute bottom-5 left-5 bg-purple-600 w-[93%] p-5'>
+                                        <div className="flex justify-between items-center w-[100%]">
+                                            <h1 className='w-2/3 font-bold text-2xl'>{item.judul}</h1>
+                                            <Link href={`/berita/${item.id}`} className='underline'>Selengkapnya</Link>
+                                        </div>
+                                    </div>
+                                </div>
+                            ))}
                         </div>
                     </div>
                 </div>
