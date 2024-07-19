182b42c55305841d611a5d02f804c28a0c4f498f
Michael-Nanpa <michaelwijayananpa320@gmail.com>
2024-07-19 15:42:44 +0700

    Update ProfileController.php

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -10,6 +10,8 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Redirect;
 use Inertia\Inertia;
 use Inertia\Response;
+use App\Models\User;
+
 
 class ProfileController extends Controller
 {
