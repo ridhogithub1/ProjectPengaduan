54609dd3d343bb9ad0013c166cba56acffdad9da
Michael-Nanpa <michaelwijayananpa320@gmail.com>
2024-07-19 13:53:25 +0700

    Update RegisteredUserController.php

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -44,6 +44,6 @@ class RegisteredUserController extends Controller
         ]);
 
         Auth::login($user);
-        return redirect(route('dashboard', absolute: false));
+       return redirect(route('dashboard_warga', absolute: false));
     }
 }
