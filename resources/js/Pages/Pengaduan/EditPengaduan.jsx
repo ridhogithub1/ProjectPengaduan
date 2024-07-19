6eeb7007c4b8d1c4f5ee6cdc3050cc708c5bf5bd
Michael-Nanpa <michaelwijayananpa320@gmail.com>
2024-07-19 15:32:07 +0700

    Update EditPengaduan.jsx

--- resources/js/Pages/Pengaduan/EditPengaduan.jsx
+++ resources/js/Pages/Pengaduan/EditPengaduan.jsx
@@ -1,7 +1,7 @@
 
 import React from 'react';
 import { useForm } from '@inertiajs/react';
-import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout_Admin';
 import InputError from '@/Components/InputError';
 import InputLabel from '@/Components/InputLabel';
 import PrimaryButton from '@/Components/PrimaryButton';
