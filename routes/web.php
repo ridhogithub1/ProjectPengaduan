199aa76605b6d44e93882bb7a04f9ab6446a228b
DhaffaMaulanaS <165637500+DhaffaMaulanaS@users.noreply.github.com>
2024-07-22 10:51:46 +0700

    Add files via upload

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\ProjectController;
 use App\Http\Controllers\TaskController;
@@ -12,36 +13,33 @@ use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 
 use Inertia\Inertia;
-
-Route::redirect('/', '/dashboard');
+Route::redirect('/', '/login');
 
 Route::middleware(['auth', 'verified'])->group(function () {
-    Route::get('/dashboard_admin', fn() => Inertia::render('Dashboard/Dashboard'))->name('dashboard');
-    Route::get('/dashboard_warga', fn() => Inertia::render('Dashboard/Dashboard_Warga'))->name('dashboard_warga');
-    Route::get('/dashboard_rt', fn() => Inertia::render('Dashboard/Dashboard_RT'))->name('dashboard_rt');
-    Route::get('/dashboard_anggota', fn() => Inertia::render('Dashboard/Dashboard_Anggota'))->name('dashboard_anggota');
+    Route::get('/dashboard_admin', [DashboardController::class,'index'])->name('dashboard');
+    Route::get('/dashboard_warga', [DashboardController::class,'dwarga'])->name('dashboard_warga');
+    Route::get('/dashboard_rt', [DashboardController::class,'drt'])->name('dashboard_rt');
+    Route::get('/dashboard_anggota', [DashboardController::class,'danggota'])->name('dashboard_anggota');
     
     Route::resource('pengaduan', PengaduanController::class);
+    
     Route::resource('diskusi', DiskusiController::class);
     Route::resource('user', UserController::class);
     Route::resource('berita', BeritaController::class);
-    Route::put('pengaduan/{id}', [PengaduanController::class, 'update'])->name('pengaduan.update');
+    Route::put('/pengaduan/{id}', [PengaduanController::class, 'update'])->name('pengaduan.update');
 
     Route::resource('project', ProjectController::class);
     Route::resource('task', TaskController::class);
+
+    Route::get('/pengaduan_warga', [PengaduanController::class, 'indexWarga'])->name('pengaduan.warga');
+    Route::get('/pengaduan_anggota', [PengaduanController::class, 'indexAnggota'])->name('pengaduan.anggota');
+    Route::get('/pengaduan_rt', [PengaduanController::class, 'index'])->name('pengaduan.rt');
+    Route::get('/pengaduan_admin', [PengaduanController::class, 'indexAdmin'])->name('pengaduan.admin');
+
+
+
     // Route::resource('user', UserController::class);
 });
-
-
-
-Route::get('/pengaduan/create', [PengaduanController::class, 'create'])->name('pengaduan.create');
-
-
-// Route::resource('user', UserController::class);
-
-// Route::middleware(['auth', 'warga'])->group(function () {
-//     Route::get('/warga-dashboard', [WargaDashboardController::class, 'index'])->name('warga.dashboard');
-// });
 
 
 Route::middleware('auth')->group(function () {
@@ -50,4 +48,4 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__ . '/auth.php';
+require __DIR__.'/auth.php';
